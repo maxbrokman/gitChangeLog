4058fc570dd6d3a82d5de974e7cfab49c37272b5
Charlie Kassel <ck@charliekassel.com>
2014-09-19 14:21:03 +0100

    added get diff functions

--- src/Git.php
+++ src/Git.php
@@ -3,15 +3,17 @@
 
 namespace MaxBrokman\GitChangeLog;
 
-class Git {
-
+class Git
+{
     private $publicMarker = "@public\\|!public";
+    private $excludeMarker = "!private";
 
     public function getTags()
     {
         $command = "git tag -l";
         exec($command, $tags);
         arsort($tags, SORT_NATURAL);
+
         return array_values($tags);
     }
 
@@ -21,15 +23,17 @@ class Git {
                 git log \\
                     --grep='$this->publicMarker' \\
                     --relative-date \\
+                    --no-merges \\
                     --pretty=format:'{ \"commit\": \"%h\",  \"author\": \"%an <%ae>\",  \"date\": \"%ad\",  \"message\": \"%s\"}' \\
-                    $from..$to
+                    $from..$to \\
+                | grep -v $this->excludeMarker
         ";
 
         exec($command, $json);
 
         $commits = [];
 
-        foreach($json as $line) {
+        foreach ($json as $line) {
             $commit = json_decode($line);
             if (json_last_error() === JSON_ERROR_NONE) {
                 $commits[] = $commit;
@@ -49,9 +53,50 @@ class Git {
     public function getFirstCommit()
     {
         $command = "git log --pretty=format:%H | tail -1";
+
         return exec($command);
     }
 
+    /**
+     * Gets the previous commit hash
+     * @param  string $commit
+     * @return string $commit - the abbrieviated commit hash of the previous commit
+     */
+    public function getPreviousCommit($commit)
+    {
+        $date = $this->getDate($commit);
+        $command = "git log --before=$date --max-count=2 --format=%h";
+        exec($command, $commits);
+
+        return $commits[1];
+    }
+
+    /**
+     * Gets diff summary since previous commit
+     * @param  string $commit
+     * @return array  $diff
+     */
+    public function getDiff($commit)
+    {
+        $previousCommit = $this->getPreviousCommit($commit);
+        $command = "git diff --stat $previousCommit $commit";
+        exec($command, $diff);
+
+        return $diff;
+    }
+
+    /**
+     * @param string $commit
+     * @param string $file
+     */
+    public function getDiffFile($commit, $file)
+    {
+        $file = str_replace('-', '/', $file);
+        $command = "git diff $commit -- $file";
+        exec($command, $diff);
+
+        return $diff;
+    }
 
     /**
      * @param string $publicMarker
@@ -62,5 +107,11 @@ class Git {
         $this->publicMarker = $publicMarker;
     }
 
-
-} 
+    /**
+     * @param string $excludeMarker
+     */
+    public function setExcludeMarker($excludeMarker)
+    {
+        $this->excludeMarker = $excludeMarker;
+    }
+}
